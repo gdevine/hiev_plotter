30126823f34a4bcd1206996fe295a44e54767719
Gerard Devine <gerarddevine@gmail.com>
2013-08-08 16:26:19 +1000

    Stipulate header rows

--- toa5_to_json.py
+++ toa5_to_json.py
@@ -7,7 +7,22 @@ Created on 08/08/2013
 import csv
 import json
 
-f = open( 'testdata/MROS_AUTO_TSOIL_R_20130807.dat', 'r' )
-reader = csv.DictReader( f, fieldnames = ( "id","name","lat","lng" ) )
-out = json.dumps( [ row for row in reader ] )
-print out
+ifile = open( 'testdata/MROS_AUTO_TSOIL_R_20130807.dat', 'rb')
+reader = csv.reader(ifile)
+ 
+rownum = 0
+for row in reader:
+    # Save header row.
+    if rownum == 1:
+        header = row
+    elif rownum > 4:
+        colnum = 0
+        for col in row:
+            #print '%-8s: %s' % (header[colnum], col)
+            print json.dumps( row, indent=4 )
+            colnum += 1
+             
+    rownum += 1
+
+ifile.close()
+print 'done'
